306b5dec77dcfacdc42f4bc7c00711157986c2eb
mvpc-11232 <mvp2612@hotmail.com>
2022-11-03 06:16:41 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
 module "vpc" {
   source                = "./vendor/modules/vpc"
-  VPC_CIDR              = var.VPC_CIDR
-  ENV                   = var.ENV
-  PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
-  AZ                    = var.AZ
-  DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
-  PRIVATE_HOSTEDZONE_ID = var.PRIVATE_HOSTEDZONE_ID
-  PRIVATE_HOSTEDZONE_NAME = var.PRIVATE_HOSTEDZONE_NAME 
-  PUBLIC_HOSTEDZONE_ID    = var.PUBLIC_HOSTEDZONE_ID
-  PUBLIC_HOSTEDZONE_NAME  = var.PUBLIC_HOSTEDZONE_NAME
 }
 
 
--- vars.tf
+++ vars.tf
@@ -1,13 +1,3 @@
 variable "VPC_CIDR" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
 variable "ENV" {}
-variable "AZ" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_RT" {}
-
-variable "PRIVATE_HOSTEDZONE_ID" {}
-variable "PRIVATE_HOSTEDZONE_NAME" {} 
-variable "PUBLIC_HOSTEDZONE_ID"  {}
-variable "PUBLIC_HOSTEDZONE_NAME" {}  
+ 

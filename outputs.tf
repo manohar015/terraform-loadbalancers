d9b24f01e8cca2171a175eeea77941010b783251
mvpc-11232 <mvp2612@hotmail.com>
2022-11-07 06:18:41 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "PUBLIC_ALB_ARN" {
     value = module.alb-public.ALB_ARN
 }
 
-output "PUBLIC_ALB_ARN" {
-    value = module.alb-public.ALB_ARN
+output "PRIVATE_ALB_ARN" {
+    value = module.alb-private.ALB_ARN
 }

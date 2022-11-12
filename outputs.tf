742ba49580539404bd9cd4a1c033a702d2c3e40c
mvpc-11232 <mvp2612@hotmail.com>
2022-11-12 06:25:42 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,14 @@ output "PRIVATE_ALB_ARN" {
 output "PRIVATE_LISTERNER_ARN" {
     value = module.alb-private.LISTENER_ARN[0]
 }
+
+output "PUBLIC_ALB_ADDRESS" {
+    value = 
+}
+
+
+
+
+# output "ALB_ADDRESS" {
+#     value = aws_lb.alb.dns_name
+# }

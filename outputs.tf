f7ff08b9ddd642b110d161b3c5423d3c9fe2ab3a
mvpc-11232 <mvp2612@hotmail.com>
2022-11-14 22:43:02 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,6 @@ output "PRIVATE_LISTERNER_ARN" {
     value = module.alb-private.LISTENER_ARN[0]
 }
 
-output "PUBLIC_LISTENER_ARN" {
-    value = module.alb-public.LISTENER_ARN[0]
-}
-
 output "PRIVATE_ALB_ADDRESS" {
     value =  module.alb-private.ALB_ADDRESS
 }
